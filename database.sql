879f6580d1feb161a8e2fc65910c1684dc8c536f
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-13 13:38:41 +0100

    Alright fixed the SQL database, thank you MySQL

--- database.sql
+++ database.sql
@@ -1,19 +1,24 @@
+-- --------------------------------------------------------
+-- 10.3.12-MariaDB               
+-- --------------------------------------------------------
+
 create database if not exists `everybodyvotes-go`;
 use `everybodyvotes-go`;
 
 create table if not exists `polls` (
-  `ID` int(5) unsigned zerofill default,
-  `Question` varchar(200) default null,
-  `A` varchar(40) default null,
-  `B` varchar(40) default null
+  `ID` int(5) unsigned not null,
+  `Question` varchar(200) not null,
+  `A` varchar(40) not null,
+  `B` varchar(40) not null
 ) engine=InnoDB default charset=utf8;
 
 create table if not exists `users` (
-  `MAC` varchar(17) NOT null
+  `Data` binary(200) DEFAULT null,
+  `MAC` varchar(17) not null
 ) engine=InnoDB default charset=utf8;
 
 create table if not exists `votes` (
-  `ID` int(5) unsigned zerofill default null,
-  `AVotes` int(3) unsigned zerofill default null,
-  `BVotes` int(3) unsigned zerofill default null
+  `ID` int(5) unsigned not null,
+  `AVotes` int(3) unsigned zerofill not null,
+  `BVotes` int(3) unsigned zerofill not null
 ) engine=InnoDB default charset=utf8;

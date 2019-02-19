cceca7fdf4eca3a4dffb97b28e33b897c2427347
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-19 20:36:57 +0100

    fixed up database and added license header to the SQL script

--- database.sql
+++ database.sql
@@ -1,3 +1,25 @@
+--		Copyright (c) 2019 IronConnect64
+--
+--		EverybodyVotes-GO, a custom server for the Everybody Votes channel on the PSP.
+--
+--	Permission is hereby granted, free of charge, to any person obtaining a copy
+--	of this software and associated documentation files (the "Software"), to deal
+--	in the Software without restriction, including without limitation the rights
+--	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--	copies of the Software, and to permit persons to whom the Software is
+--	furnished to do so, subject to the following conditions:
+--
+--	The above copyright notice and this permission notice shall be included in all
+--	copies or substantial portions of the Software.
+--
+--	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--	SOFTWARE.
+--
 -- --------------------------------------------------------
 -- 10.3.12-MariaDB               
 -- --------------------------------------------------------
@@ -6,19 +28,19 @@ create database if not exists `everybodyvotes-go`;
 use `everybodyvotes-go`;
 
 create table if not exists `polls` (
-  `ID` int(5) unsigned not null,
-  `Question` varchar(200) not null,
-  `A` varchar(40) not null,
-  `B` varchar(40) not null
+  `ID` int(5) unsigned,
+  `Question` varchar(200),
+  `A` varchar(40),
+  `B` varchar(40)
 ) engine=InnoDB default charset=utf8;
 
 create table if not exists `users` (
-  `Data` binary(200) DEFAULT null,
-  `MAC` varchar(17) not null
+  `Data` binary(200) DEFAULT,
+  `MAC` varchar(17)
 ) engine=InnoDB default charset=utf8;
 
 create table if not exists `votes` (
-  `ID` int(5) unsigned not null,
-  `AVotes` int(3) unsigned zerofill not null,
-  `BVotes` int(3) unsigned zerofill not null
+  `ID` int(5) unsigned,
+  `AVotes` int(3) unsigned zerofill,
+  `BVotes` int(3) unsigned zerofill
 ) engine=InnoDB default charset=utf8;
